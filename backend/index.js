0621747c98785699a9fe2feb7a327d86818617bc
shubhank hiremath <shubhankhiremath@gmail.com>
2025-07-18 00:45:56 +0530

    Did modifications in cookie backend

--- backend/index.js
+++ backend/index.js
@@ -37,8 +37,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 1000 * 60 * 60 * 24 // 1 day
   }
 }));
